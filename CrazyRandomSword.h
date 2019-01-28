dd1f982718da2753c891540eafa56493a2ca4650
Joshua Brandon Carbee <carbee@linprog1.cs.fsu.edu>
2019-01-28 17:46:42 -0500

    Created CrazyRandomSword.h

---
+++ CrazyRandomSword.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Joshua Carbee
+ * 
+ * Created on January 28th, 2019  2:38 PM
+ */
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+/**
+ * Defines the behavior of a crazy random sword that chooses random numbers
+ */
+class CrazyRandomSword : public Weapon {
+public:
+
+    CrazyRandomSword() : Weapon("Crazy random sword", (rand() % 93 + 7) //Calls the constructor with string Crazy random sword and a random number between 7 and 100
+    {
+    }
+
+    virtual ~CrazyRandomSword() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* CRAZYRANDOMSWORD_H */
+
